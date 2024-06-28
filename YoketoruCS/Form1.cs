08fd1694e38c2c67e2c5f0bedefdb57fb307e29b
YuTanaka <tanakaedu@users.noreply.github.com>
2024-06-28 13:44:53 +0900

    ゲームオーバーのループ

--- YoketoruCS/Form1.cs
+++ YoketoruCS/Form1.cs
@@ -1,7 +1,13 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Runtime.InteropServices;
+
 namespace YoketoruCS
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(int vKey);
+
         enum State
         {
             None = -1,
@@ -45,6 +51,9 @@ namespace YoketoruCS
             switch (currentState)
             {
                 case State.Title:
+                    labelTitle.Visible = true;
+                    buttonStart.Visible = true;
+
                     labelGameover.Visible = false;
                     buttonToTitle.Visible = false;
                     break;
@@ -53,6 +62,12 @@ namespace YoketoruCS
                     labelTitle.Visible = false;
                     buttonStart.Visible = false;
                     break;
+
+                case State.Gameover:
+                    labelGameover.Visible = true;
+                    buttonToTitle.Visible = true;
+                    break;
+
             }
         }
 
@@ -68,7 +83,10 @@ namespace YoketoruCS
 
         void UpdateGame()
         {
-
+            if (GetAsyncKeyState((int)Keys.O) < 0)
+            {
+                nextState = State.Gameover;
+            }
         }
 
         private void buttonStart_Click(object sender, EventArgs e)
@@ -78,7 +96,7 @@ namespace YoketoruCS
 
         private void buttonToTitle_Click(object sender, EventArgs e)
         {
-
+            nextState = State.Title;
         }
     }
 }
