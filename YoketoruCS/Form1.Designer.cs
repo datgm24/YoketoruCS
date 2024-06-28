a96cced175cf14f561b4a0444a84a51768d01c6b
YuTanaka <tanakaedu@users.noreply.github.com>
2024-06-28 14:10:25 +0900

    ゲーム画面モック

--- YoketoruCS/Form1.Designer.cs
+++ YoketoruCS/Form1.Designer.cs
@@ -35,6 +35,11 @@
             labelGameover = new Label();
             buttonToTitle = new Button();
             labelClear = new Label();
+            labelScore = new Label();
+            labelTime = new Label();
+            tempPlayer = new Label();
+            tempItem = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // timer1
@@ -95,11 +100,68 @@
             labelClear.TabIndex = 4;
             labelClear.Text = "CLEAR!!";
             // 
+            // labelScore
+            // 
+            labelScore.AutoSize = true;
+            labelScore.Font = new Font("源ノ角ゴシック Code JP H", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            labelScore.Location = new Point(218, 9);
+            labelScore.Name = "labelScore";
+            labelScore.Size = new Size(29, 32);
+            labelScore.TabIndex = 5;
+            labelScore.Text = "0";
+            // 
+            // labelTime
+            // 
+            labelTime.AutoSize = true;
+            labelTime.Font = new Font("源ノ角ゴシック Code JP H", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            labelTime.Location = new Point(369, 226);
+            labelTime.Name = "labelTime";
+            labelTime.Size = new Size(83, 46);
+            labelTime.TabIndex = 6;
+            labelTime.Text = "200";
+            // 
+            // tempPlayer
+            // 
+            tempPlayer.AutoSize = true;
+            tempPlayer.Font = new Font("源ノ角ゴシック Code JP H", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            tempPlayer.Location = new Point(35, 207);
+            tempPlayer.Name = "tempPlayer";
+            tempPlayer.Size = new Size(75, 23);
+            tempPlayer.TabIndex = 7;
+            tempPlayer.Text = "(・ω・)";
+            // 
+            // tempItem
+            // 
+            tempItem.AutoSize = true;
+            tempItem.Font = new Font("源ノ角ゴシック Code JP H", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            tempItem.ForeColor = Color.FromArgb(255, 128, 0);
+            tempItem.Location = new Point(128, 226);
+            tempItem.Name = "tempItem";
+            tempItem.Size = new Size(36, 32);
+            tempItem.TabIndex = 8;
+            tempItem.Text = "★";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("源ノ角ゴシック Code JP H", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.Purple;
+            label3.Location = new Point(218, 226);
+            label3.Name = "label3";
+            label3.Size = new Size(26, 23);
+            label3.TabIndex = 9;
+            label3.Text = "◆";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(464, 281);
+            Controls.Add(label3);
+            Controls.Add(tempItem);
+            Controls.Add(tempPlayer);
+            Controls.Add(labelTime);
+            Controls.Add(labelScore);
             Controls.Add(labelClear);
             Controls.Add(buttonToTitle);
             Controls.Add(buttonStart);
@@ -119,5 +181,10 @@
         private Label labelGameover;
         private Button buttonToTitle;
         private Label labelClear;
+        private Label labelScore;
+        private Label labelTime;
+        private Label tempPlayer;
+        private Label tempItem;
+        private Label label3;
     }
 }
